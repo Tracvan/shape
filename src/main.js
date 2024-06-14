3c96a6d77a75076fae9338a69679fd914d2ea3b5
Trac Van <truongtracvan@gmail.com>
2024-06-14 17:22:12 +0700

    typescipt objec, interface

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var Rectangle_1 = require("./Rectangle");
+var rectangle = new Rectangle_1.Rectangle(5, 5, 10, 20);
+console.log(rectangle.toString());
+console.log(rectangle.area());
